7f369331803ce5852494f80aed9aaf6462660c47
Darren Dormer <me@darren.io>
2015-10-15 21:16:03 +0200

    Use  instead of . Make  a private def.

--- src/clj_dynamo/shortener.clj
+++ src/clj_dynamo/shortener.clj
@@ -3,13 +3,13 @@
            [org.httpkit.client :as http]))
 
 
-(def api-uri "https://api-ssl.bitly.com/v3/shorten?access_token=%s&domain=bit.ly&longUrl=%s")
+(def ^:private api-uri "https://api-ssl.bitly.com/v3/shorten?access_token=%s&domain=bit.ly&longUrl=%s")
 
 
 (defn handle-response
   "Returns the URL from the bitly response."
   [{body :body error :error}]
-  (if-not (nil? error)
+  (if error
     (println error)
     (get-in (json/parse-string body true) [:data :url])))
 
@@ -17,8 +17,8 @@
 (defn shorten-url
   "Uses Bit.ly to shorten a URL."
   [access-token url]
-  (let [response (http/get (format api-uri access-token url))]
-    (let [short-url (handle-response @response)]
-      (if (nil? short-url)
-        url
-        short-url))))
+  (let [response (http/get (format api-uri access-token url))
+        short-url (handle-response @response)]
+    (if short-url
+      short-url
+      url)))
